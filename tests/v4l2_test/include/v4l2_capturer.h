758dfb00a91c421ddc9f099aced9a9b625290c7b
chengxiaoyu <writeforever@foxmail.com>
2021-11-11 18:37:22 +0800

    update v4l2_capturer.

--- tests/v4l2_test/include/v4l2_capturer.h
+++ tests/v4l2_test/include/v4l2_capturer.h
@@ -22,44 +22,49 @@
 #include <linux/videodev2.h>
 
 #define DEFAULT_CAMERA_PATH "/dev/video0"
+#define DEFAULT_BUFFER_COUNT 4
 
 #define LOG_DEBUG(msg)printf(msg"\n")
 #define CHECK_FMT_IF_SUPPORTED(pixelformat, v4l2_fmt)if(pixelformat == v4l2_fmt)printf(#v4l2_fmt" supported\n");
 
-struct video_buffer{
-	unsigned int length;
-	unsigned int offset;
-	unsigned char *start;
+struct video_buffer {
+    unsigned int length;
+    unsigned int offset;
+    unsigned char *start;
 };
 
 using namespace std;
 
-class v4l2_capturer{
+class v4l2_capturer
+{
 public:
-	
-	v4l2_capturer();
-	~v4l2_capturer();
-	int init();
-	int start();
-	int stop();
-	int get_frame();
-	int save_fbdata_to_file(const char *path);
-	void query_supported_format();
-	bool frameSaveImage(const char * file_path);
+
+    v4l2_capturer();
+    ~v4l2_capturer();
+    int init();
+    int start();
+    int stop();
+    int get_frame();
+    int save_fbdata_to_file(const char *path);
+    int save_fbdata_to_file_by_mmap(const char *path);
+    void query_supported_format();
+    void query_supported_format_new();
+
+    bool frameSaveImage(const char *file_path);
 
 private:
-	int                        m_fd;
-	int                        m_rb_count;
-	int                        m_rb_current;
-	int                        m_total_bytes;
-	struct v4l2_capability     m_cap;
-	struct v4l2_format         m_fmt;
-	struct v4l2_fmtdesc        m_desc_fmt;
-	struct v4l2_requestbuffers m_rb;
-	struct v4l2_buffer         m_buf;
-	struct v4l2_frmsizeenum    m_frmsize;
-	struct video_buffer       *m_video_buffers;
-	unsigned int			   m_video_buff_size;
+    int                        m_fd;
+    int                        m_rb_count;
+    int                        m_rb_current;
+    int                        m_total_bytes;
+    struct v4l2_capability     m_cap;
+    struct v4l2_format         m_fmt;
+    struct v4l2_fmtdesc        m_desc_fmt;
+    struct v4l2_requestbuffers m_rb;
+    struct v4l2_buffer         m_buf;
+    struct v4l2_frmsizeenum    m_frmsize;
+    struct video_buffer       *m_video_buffers;
+    unsigned int               m_video_buff_size;
 };
 
 
--- tests/v4l2_test/src/main.cpp
+++ tests/v4l2_test/src/main.cpp
@@ -4,17 +4,20 @@
 
 int main(int argc, char const *argv[])
 {
-    std::cout<< "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    v4l2_capturer capturer;
+    capturer.init();
+    capturer.query_supported_format_new();
+    capturer.start();
 
-	v4l2_capturer capturer;
-	//capturer.query_supported_format();
-	capturer.init();
-	capturer.start();
-	capturer.get_frame();
-	capturer.save_fbdata_to_file("./out.jpg");
-	
-	//capturer.frameSaveImage("./out2.jpg");
-	capturer.stop();
-	
+	int i=25;
+	char filename[10];
+
+	while(1){
+	    capturer.get_frame();
+	}
+	/*capturer.get_frame();
+    capturer.save_fbdata_to_file_by_mmap("./out.jpg");*/
+    capturer.stop();
     return 0;
 }
