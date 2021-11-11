b5cd2f43ecc1a7e47578448a41d7f657c58bc206
jensenhua <1657802074@qq.com>
2021-11-12 00:23:26 +0800

    add signal SIGINT handler to release v4l2capturer.

--- tests/v4l2_test/src/main.cpp
+++ tests/v4l2_test/src/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <time.h>
+#include <signal.h>
 #include "v4l2_capturer.h"
+
+v4l2_capturer capturer;
+
+void capturer_sigint_handler(int signal)
+{
+	capturer.stop();
+	exit(1);
+}
 
 /* TODO: listen SIGINT and call capturer.stop() when it come. */
 int main(int argc, char const *argv[])
 {
+	int frame_count=0;
     std::cout << "Hello World!" << std::endl;
-    v4l2_capturer capturer;
+
+	/* register signal handler function. */
+	signal(SIGINT, capturer_sigint_handler);
+
     capturer.init();
     capturer.query_supported_format_new();
     capturer.start();
@@ -15,7 +28,8 @@ int main(int argc, char const *argv[])
 	char filename[10];
 
 	while(1){
-	    capturer.get_frame();
+	    frame_count=capturer.get_frame();
+	    if(frame_count==25*3)break;
 	}
 	/*capturer.get_frame();
     capturer.save_fbdata_to_file_by_mmap("./out.jpg");*/
